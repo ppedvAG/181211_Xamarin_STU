713b8c684f4ce24903790b58158f70cb0930e006
Michael Zöhling <Michael.Zoehling@outlook.com>
2018-12-12 09:07:00 +0100

    Hello Kitten

---
+++ HelloKitten/HelloKitten/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloKitten
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSuchen_Clicked(object sender, EventArgs e)
+        {
+            imageBild.Source = ImageSource.FromUri(new Uri($"http://fillmurray.com/{entryBreite.Text}/{entryHöhe.Text}"));
+        }
+    }
+}
