ac3cb2e88f5a895b93cb66e101fa291e32258746
Michael Zöhling <Michael.Zoehling@outlook.com>
2018-12-13 15:23:20 +0100

    Binding Beispiel

---
+++ DataBindingDemo/DataBindingDemo/FaktorKonverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DataBindingDemo
+{
+    class FaktorKonverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int wert = System.Convert.ToInt32(value);
+            int faktor = System.Convert.ToInt32((parameter as Entry).Text);
+            wert *= faktor;
+            return wert;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataBindingDemo/DataBindingDemo/FarbKonverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DataBindingDemo
+{
+    class Farbkonverter : IValueConverter
+    {
+        FieldInfo[] fields = typeof(Color).GetFields();
+        Dictionary<string, string> localizedColors = new Dictionary<string, string>
+        {
+            {"Blau","Blue" },
+            {"Rot","Red" },
+            {"Gelb","Yellow" }
+        };
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            FieldInfo targetColor;
+            if (localizedColors.ContainsKey(value.ToString()))
+                targetColor = fields.FirstOrDefault(x => x.Name == localizedColors[value.ToString()]);
+            else
+                targetColor = fields.FirstOrDefault(x => x.Name == value.ToString());
+
+            if (value.ToString().StartsWith("#"))
+                return Color.FromHex(value.ToString());
+            if (targetColor == null)
+                return Color.Black;
+            else
+            {
+                return targetColor.GetValue(typeof(Color));
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
